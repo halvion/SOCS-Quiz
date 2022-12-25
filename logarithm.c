e851f93a21067d0b20a2605a79455c34804e9fea
Matthew <68104729+halvion@users.noreply.github.com>
2022-12-26 00:49:18 +0700

    Problem E

--- logarithm.c
+++ logarithm.c
@@ -4,7 +4,8 @@
 int logarithma(float varX, float varY){
 
     float result;
-    // result adalah hasil dari nilai terbesar yang lebih kecil atau sama dengan dari pembagian log(a)/log(b) yang menhasilkan N dari b^N  <= a
+    /* result adalah hasil dari nilai terbesar yang lebih kecil atau sama dengan dari pembagian 
+    log(a)/log(b) yang menhasilkan N dari b^N  <= a */
     float a = log10(varY)/log10(varX);
     result = ceil(floor(a));
     return result;
